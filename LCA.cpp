428f0ec4a9c1a594aad73b21c985859be599be8c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-05 04:17:40 -0500

    Implemented unload function to free memory

--- LCA.cpp
+++ LCA.cpp
@@ -12,6 +12,7 @@ struct Node{
 
 Node *tree(int *num, int len, int indx);
 Node *LCA(Node *root, int node1, int node2);
+void unload(Node *root);
 
 int main() {
 	// Create tree
@@ -19,7 +20,7 @@ int main() {
 	int nums[LEN] = {3, 11, 5, 2, 6, 17, 4, 7, 9, 13, 21, 14, 8, 19, 1};
 	Node *root = tree(nums, LEN, 0);
 
-
+	// Print Lowest common ancestor.
 	Node *lca = LCA(root, 7, 9);
 
 	cout << lca->value << endl; // 2
@@ -40,6 +41,8 @@ int main() {
 
 	cout << lca->value << endl; // 11
 
+	// Unload Dynamically allocated memory.
+	unload(root);
 
 };
 
@@ -62,25 +65,36 @@ Node *tree(int *num, int len, int indx) {
 	return node;
 
 };
-
+// Finds Lowest Common Ancestor
 Node *LCA(Node *root, int node1, int node2) {
+	// End of tree is reached
 	if(!root)
 		return NULL;
+	// Return the node if it's equal to either node 1 or 2.
 	if(root->value == node1 || root->value == node2)
 		return root;
-	
+	// Traverse Through the tree either until we find the nodes or the end is reached.
 	Node *left = LCA(root->left, node1, node2);
 	Node *right = LCA(root->right, node1, node2);
 	
-
+	// If left and right are not NULL then the current root value is the LCA.
 	if(left && right)
 		return root;
 
-
+	// Either return left, right, or null.
 	return left ? left : right;
 
 };
 
+// Free Dynamic memory.
+void unload(Node *root) {
+	if(!root)
+		return;
+
+	unload(root->left);
+	unload(root->right);
+	delete root;
+};
 
 
 
