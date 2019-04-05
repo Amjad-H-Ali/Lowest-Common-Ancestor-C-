604ce6cae8843e0b71face5dfc3ca8476ccb321b
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-05 00:44:02 -0500

    LCA function returns Lowest common parent of given nodes

--- LCA.cpp
+++ LCA.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// For binary tree
 struct Node{
 	int value;
 	Node *left;
@@ -10,16 +11,39 @@ struct Node{
 };
 
 Node *tree(int *num, int len, int indx);
+Node *LCA(Node *root, int node1, int node2);
 
 int main() {
+	// Create tree
 	const int LEN = 15;
-	int nums[LEN] = {3, 11, 5, 2, 6, 17, 4, 7, 9, 13, 21, 14, 8, 19, 21};
+	int nums[LEN] = {3, 11, 5, 2, 6, 17, 4, 7, 9, 13, 21, 14, 8, 19, 1};
 	Node *root = tree(nums, LEN, 0);
 
-	cout << root->left->right->right->value << '\n';
+
+	Node *lca = LCA(root, 7, 9);
+
+	cout << lca->value << endl; // 2
+
+	lca = LCA(root, 6, 13);
+
+	cout << lca->value << endl; // 6
+
+	lca = LCA(root, 4, 6);
+
+	cout << lca->value << endl; // 3
+
+	lca = LCA(root, 17, 4);
+
+	cout << lca->value << endl; // 5
+
+	lca = LCA(root, 21, 2);
+
+	cout << lca->value << endl; // 11
+
 
 };
 
+// Creates tree from array using recursion
 Node *tree(int *num, int len, int indx) {
 	if(indx >= len)
 		return NULL;
@@ -38,3 +62,28 @@ Node *tree(int *num, int len, int indx) {
 	return node;
 
 };
+
+Node *LCA(Node *root, int node1, int node2) {
+	if(!root)
+		return NULL;
+	if(root->value == node1 || root->value == node2)
+		return root;
+	
+	Node *left = LCA(root->left, node1, node2);
+	Node *right = LCA(root->right, node1, node2);
+	
+
+	if(left && right)
+		return root;
+
+
+	return left ? left : right;
+
+};
+
+
+
+
+
+
+
